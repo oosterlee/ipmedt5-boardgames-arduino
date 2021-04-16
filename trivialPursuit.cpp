ee546a928e65c23825a98491b4d1454b2b8ca340
Kasper <s1118202@student.hsleiden.nl>
2021-04-16 15:11:25 +0200

    tp test

--- trivialPursuit.cpp
+++ trivialPursuit.cpp
@@ -7,6 +7,13 @@ private:
 	SocketIoClient* socket;
 	Adafruit_NeoPixel* strip;
 	int gameId = -1;
+  int positions[20] = {9, 10, 11, 12, 13, 14, 17, 30, 33, 46, 49, 50, 51, 52, 53, 54, 41, 38, 25, 22};
+  int players [4] = {0, 0, 0, 0};
+  const int p1 = 13;
+  const int p2 = 12;
+  const int p3 = 33;
+  const int p4 = 32;
+
 public:
 	void setSocket(SocketIoClient* s) {this->socket = s;}
 	void setStrip(Adafruit_NeoPixel* strip) {this->strip = strip;}
@@ -14,9 +21,33 @@ public:
 
 	void setup() {
 		Serial.println("Trivial pursuit!!");
+		strip->begin();
+		strip->setBrightness(240); // 1/3 brightness
 	}
 
 	void loop() {
-
+  websocket
+  if(touchRead(p1) < 25){
+    strip->setPixelColor(positions[players[0]], 0,0,0);
+    players[0] = players[0] + 1;
+    strip->setPixelColor(positions[players[0]], 50,0,0);
+  }
+  else if(touchRead(p2) < 25){
+    strip->setPixelColor(positions[players[1]], 0,0,0);
+    players[1] = players[1] + 1;
+    strip->setPixelColor(positions[players[1]], 0,0,50);
+  }
+  else if(touchRead(p3) < 25){
+    strip->setPixelColor(positions[players[2]], 0,0,0);
+    players[2] = players[2] + 1;
+    strip->setPixelColor(positions[players[2]], 0,50,0);
+  }
+  else if(touchRead(p4) < 25){
+    strip->setPixelColor(positions[players[3]], 0,0,0);
+    players[3] = players[3] + 1;
+    strip->setPixelColor(positions[players[3]], 50,50,0);
+  }
+  strip->show();
+  delay(1000);
 	}
 };
--- vlotteGeesten.cpp
+++ vlotteGeesten.cpp
@@ -7,73 +7,16 @@ private:
 	SocketIoClient* socket;
 	Adafruit_NeoPixel* strip;
 	int gameId = -1;
-  const int ledPin = 23; // the number of the neopixel strip
-  const int numLeds = 64;
-  int LDR_VALUE__ONE;
-  int LDR_VALUE__TWO;
-  int LDR_VALUE__THREE;
-  int LDR_VALUE__FOUR;
-  int LDR_VALUE__FIVE;
-  
 public:
 	void setSocket(SocketIoClient* s) {this->socket = s;}
 	void setStrip(Adafruit_NeoPixel* strip) {this->strip = strip;}
 	void setGameId(int gameId) {if (this->gameId != gameId) this->gameId = gameId;}
 
 	void setup() {
-      Serial.println("Vlotte geesten!");
-  }
+		Serial.println("Vlotte geesten!");
+	}
 
-    void loop() {
-
-      Serial.println("Hello WOrld");
-      LDR_VALUE__ONE = analogRead(25);
-      LDR_VALUE__TWO = analogRead(26);
-      LDR_VALUE__THREE = analogRead(34);
-      LDR_VALUE__FOUR = analogRead(35);
-      LDR_VALUE__FIVE = analogRead(39);
-
-      if(LDR_VALUE__ONE < 600){
-        
-        strip->setPixelColor(19, 0, 0, 255);
-      
-      }else{
-        strip->setPixelColor(19, 0, 0,0);
-      }
-
-      if(LDR_VALUE__TWO < 600){
-
-        strip->setPixelColor(26, 255, 0,0);
-      
-      }else{
-        strip->setPixelColor(26, 0, 0,0);
-      }
-
-      if(LDR_VALUE__THREE < 600){
-        
-        strip->setPixelColor(33, 0, 128, 0);
-      
-      }else{
-        strip->setPixelColor(33, 0, 0,0);
-      }
-
-      if(LDR_VALUE__FOUR < 600){
-
-        strip->setPixelColor(42, 138, 186,211);
-      
-      }else{
-        strip->setPixelColor(42, 0, 0,0);
-      }
-
-      if(LDR_VALUE__FIVE < 600){
-
-        strip->setPixelColor(51, 255, 255,255);
-      
-      }else{
-        strip->setPixelColor(51, 0, 0,0);
-      }
-      
-      strip->show();
-    }
+	void loop() {
   
+	}
 };
